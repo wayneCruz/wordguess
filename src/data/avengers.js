e9d03875f87ef34240c8d790b8d3bc5e06bf9e49
wayneCruz <84614229+wayneCruz@users.noreply.github.com>
2025-04-18 04:00:16 +0800

    Update avengers.js

--- src/data/avengers.js
+++ src/data/avengers.js
@@ -1,55 +1,66 @@
+import spiderman from "../assets/avengers/spiderman.png"
+import doctorstrage from "../assets/avengers/doctorstrange.png"
+import hawkeye from "../assets/avengers/hawkeye.png"
+import blackwidow from "../assets/avengers/blackwidow.png"
+import hulk from "../assets/avengers/hulk.png"
+import captainamerica from "../assets/avengers/captainamerica.png"
+import thor from "../assets/avengers/thor.png"
+import ironman from "../assets/avengers/ironman.png"
+import thanos from "../assets/avengers/thanos.png"
+
+
 export const avengers = [
   {
       name: "Spider Man",
-      image: "assets/avengers/spiderman.png",
+      image: spiderman,
       backgroundColor: "#E2680F",
       color: "#F9F4DA",
   },
   {
       name: "Doctor Strange",
-      image: "assets/avengers/doctorstrange.png",
+      image: doctorstrage,
       backgroundColor: "#328AF1",
       color: "#F9F4DA",
   },
   {
       name: "Hawkeye",
-      image: "assets/avengers/hawkeye.png",
+      image: hawkeye,
       backgroundColor: "#F4EB13",
       color: "#1E1E1E",
   },
   {
       name: "Black Widow",
-      image: "assets/avengers/blackwidow.png",
+      image: blackwidow,
       backgroundColor: "#2ED3E9",
       color: "#1E1E1E",
   },
   {
       name: "Hulk",
-      image: "assets/avengers/hulk.png",
+      image: hulk,
       backgroundColor: "#298EC6",
       color: "#F9F4DA",
   },
   {
       name: "Captain America",
-      image: "assets/avengers/captainamerica.png",
+      image: captainamerica,
       backgroundColor: "#599137",
       color: "#F9F4DA",
   },
   {
       name: "Thor",
-      image: "assets/avengers/thor.png",
+      image: thor,
       backgroundColor: "#FFD742",
       color: "#1E1E1E",
   },
   {
       name: "Iron Man",
-      image: "assets/avengers/ironman.png",
+      image: ironman,
       backgroundColor: "#D02B2B",
       color: "#F9F4DA",
   },
   {
       name: "Thanos",
-      image: "assets/avengers/thanos.png",
+      image: thanos,
       backgroundColor: "#2D519F",
       color: "#F9F4DA",
   },
